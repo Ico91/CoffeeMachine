692a636d13b82b5584098f11bfb530f742dd1922
stinkybh <stkgstudio@gmail.com>
2013-07-15 12:49:13 +0300

    Resolving main

--- src/coffeeMachine/main/Main.java
+++ src/coffeeMachine/main/Main.java
@@ -1,11 +1,5 @@
 package coffeeMachine.main;
 
-<<<<<<< HEAD
-=======
-import java.util.HashMap;
-import java.util.Map;
-
->>>>>>> 9550f8bcba9e846f54a93883bc605fa9ecdd9316
 public class Main {
 	
 	public static void main(String[] args) {
