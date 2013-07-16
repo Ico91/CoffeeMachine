232de04d3b2e25c7e036df9316a2bcfa1d6391e3
stinkybh <stkgstudio@gmail.com>
2013-07-16 14:47:04 +0300

    OrderFinalizeFlow commit

---
+++ src/coffeeMachine/main/OrderFinalizeFlow.java
@@ -0,0 +1,103 @@
+package coffeeMachine.main;
+
+import java.util.Map;
+
+/***
+ * Finalizing order and show information about ordered drink and change to the
+ * user
+ * 
+ * @author Krasimir Nikolov Atanasov
+ * 
+ */
+public class OrderFinalizeFlow implements Flow {
+
+	private Drink drink;
+	private Withdraw change;
+
+	/***
+	 * 
+	 * @param drink
+	 *            - Ordered drink
+	 * @param change
+	 *            - Returned change
+	 */
+	public OrderFinalizeFlow(Drink drink, Withdraw change) {
+		this.drink = drink;
+		this.change = change;
+	}
+
+	@Override
+	public Flow execute(CoffeeMachineState machine) {
+		printOrderInformation();
+		finalizeDrinkOrder(machine);
+		removeChangeFromMachine(machine);
+
+		return new DrinksListFlow();
+	}
+
+	void removeChangeFromMachine(CoffeeMachineState machine) {
+		MoneyAmount moneyInMachine = machine.getCoins();
+		moneyInMachine.withdraw(this.change.getChange().getSumOfCoinsValue());
+	}
+
+	private void printOrderInformation() {
+		System.out.println("Your " + this.drink.getName() + " is ready.");
+		if (this.change.getStatus() == RequestResultStatus.SUCCESSFUL) {
+			System.out.println("Your change is: "
+					+ this.change.getChange().toString());
+		} else {
+			System.out.println("You receive: "
+					+ this.change.getChange().toString() + " as change.");
+		}
+	}
+
+	void finalizeDrinkOrder(CoffeeMachineState machine) {
+		DrinksContainer drinksContainer = machine.getDrinks();
+		Map<Drink, Integer> drinks = drinksContainer.getDrinks();
+		int countOfSelectedDrink = drinks.get(drink);
+
+		countOfSelectedDrink--;
+		drinks.put(this.drink, countOfSelectedDrink);
+
+		drinksContainer.setDrinks(drinks);
+		machine.setDrinks(drinksContainer);
+	}
+
+	/***
+	 * Getter for ordered drink
+	 * 
+	 * @return ordered drink
+	 */
+	public Drink getDrink() {
+		return drink;
+	}
+
+	/***
+	 * Setter for ordered drink
+	 * 
+	 * @param drink
+	 *            - ordered drink
+	 */
+	public void setDrink(Drink drink) {
+		this.drink = drink;
+	}
+
+	/***
+	 * Getter for returned change
+	 * 
+	 * @return Withdraw object containing returned change
+	 */
+	public Withdraw getChange() {
+		return change;
+	}
+
+	/***
+	 * Setter for returned change
+	 * 
+	 * @param Withdraw
+	 *            object containing returned change
+	 */
+	public void setChange(Withdraw change) {
+		this.change = change;
+	}
+}
---
+++ tests/coffeeMachine/main/OrderFinalizeFlowTests.java
@@ -0,0 +1,55 @@
+package coffeeMachine.main;
+
+import static org.junit.Assert.*;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class OrderFinalizeFlowTests {
+	private CoffeeMachineState coffeeMachine;
+	private OrderFinalizeFlow orderFinalizeFlow;
+
+	@Before
+	public void testMoneyAmount_SetUpObject() {
+		Drink drink = new Drink("Coffee", 30);
+		Map<Coin, Integer> coins = new TreeMap<Coin, Integer>();
+		coins.put(Coin.FIVE, 5);
+		coins.put(Coin.TEN, 4);
+		coins.put(Coin.TWENTY, 3);
+		coins.put(Coin.FIFTY, 2);
+		coins.put(Coin.LEV, 1);
+
+		Map<Drink, Integer> availableDrinks = new TreeMap<Drink, Integer>();
+		availableDrinks.put(drink, 50);
+		availableDrinks.put(new Drink("Tea", 40), 10);
+		availableDrinks.put(new Drink("Hot chocolate", 50), 5);
+
+		MoneyAmount availableCoins = new MoneyAmount(coins);
+		this.coffeeMachine = new CoffeeMachineState(availableCoins,
+				new DrinksContainer(availableDrinks));
+	}
+
+	@Test
+	public void testFinalizeDrinkOrderMustReturnCorrectAmountOfDrinks() {
+		Map<Coin, Integer> coins = new TreeMap<Coin, Integer>();
+		coins.put(Coin.FIVE, 2);
+		coins.put(Coin.TEN, 2);
+		coins.put(Coin.TWENTY, 0);
+		coins.put(Coin.FIFTY, 0);
+		coins.put(Coin.LEV, 0);
+		
+		MoneyAmount change = new MoneyAmount(coins);
+		Withdraw withdraw = change.withdraw(30);
+		Drink drink = new Drink("Coffee", 30);
+		
+		orderFinalizeFlow = new OrderFinalizeFlow(drink, withdraw);
+		orderFinalizeFlow.finalizeDrinkOrder(coffeeMachine);
+		int expected = 49;
+		int actual = coffeeMachine.getDrinks().getDrinks().get(drink);
+		
+		assertTrue(expected == actual);
+	}
+}
