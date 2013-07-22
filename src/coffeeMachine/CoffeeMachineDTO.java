2b2f3354ba4825e3194c5aa6a5fa8e01dd62011f
stinkybh <stkgstudio@gmail.com>
2013-07-22 10:35:34 +0300

    Removed imports

--- src/coffeeMachine/CoffeeMachineDTO.java
+++ src/coffeeMachine/CoffeeMachineDTO.java
@@ -1,11 +1,7 @@
 package coffeeMachine;
 
-
 import java.io.File;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -13,62 +9,59 @@ import javax.xml.bind.Unmarshaller;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
-
-
 @XmlRootElement
-public class CoffeeMachineDTO  {
+public class CoffeeMachineDTO {
 	@XmlElement
 	ArrayList<DrinkDTO> drinksDTO;
-	
-	CoffeeMachineDTO(){
-		drinksDTO=new ArrayList<DrinkDTO>();
+
+	CoffeeMachineDTO() {
+		drinksDTO = new ArrayList<DrinkDTO>();
 	}
-	
-	
-	public void setDrinksDTO(ArrayList<DrinkDTO> drinkDTO){
-		this.drinksDTO=drinkDTO;
+
+	public void setDrinksDTO(ArrayList<DrinkDTO> drinkDTO) {
+		this.drinksDTO = drinkDTO;
 	}
-	
+
 	@Override
-	public String toString(){
-		String str=" ";
-		for(int i=0;i<drinksDTO.size();i++){
-			str+=drinksDTO.get(i).toString();
+	public String toString() {
+		String str = " ";
+		for (int i = 0; i < drinksDTO.size(); i++) {
+			str += drinksDTO.get(i).toString();
 		}
 		return str;
 	}
-	public ArrayList<DrinkDTO> getdrinksDTO(){
+
+	public ArrayList<DrinkDTO> getdrinksDTO() {
 		return drinksDTO;
 	}
-	
-	public void UnmarshallingDrinks(){
-		try {	
-			 
+
+	public void UnmarshallingDrinks() {
+		try {
+
 			File file = new File("file.xml");
-			JAXBContext jaxbContext = JAXBContext.newInstance(CoffeeMachineDTO.class);
-	 
+			JAXBContext jaxbContext = JAXBContext
+					.newInstance(CoffeeMachineDTO.class);
+
 			Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-			CoffeeMachineDTO cfdOut = (CoffeeMachineDTO) jaxbUnmarshaller.unmarshal(file);
+			CoffeeMachineDTO cfdOut = (CoffeeMachineDTO) jaxbUnmarshaller
+					.unmarshal(file);
 			this.setDrinksDTO(cfdOut.getdrinksDTO());
-			//System.out.println(cfdOut);
-	 
-		  } catch (JAXBException e) {
+			// System.out.println(cfdOut);
+
+		} catch (JAXBException e) {
 			e.printStackTrace();
-		  }
-	 
 		}
-	
-	public DrinksContainer getDrinksContainer(){
-		DrinksContainer drinksContainer=new DrinksContainer();
-		
-		
-		for(int i=0;i<drinksDTO.size();i++){
-			drinksContainer.add(new Drink(drinksDTO.get(i).getName(),drinksDTO.get(i).getPrice()), drinksDTO.get(i).getAmount());
+
+	}
+
+	public DrinksContainer getDrinksContainer() {
+		DrinksContainer drinksContainer = new DrinksContainer();
+
+		for (int i = 0; i < drinksDTO.size(); i++) {
+			drinksContainer.add(new Drink(drinksDTO.get(i).getName(), drinksDTO
+					.get(i).getPrice()), drinksDTO.get(i).getAmount());
 		}
 		return drinksContainer;
-		}
-		
+	}
 
-	
-	
 }
--- src/coffeeMachine/Main.java
+++ src/coffeeMachine/Main.java
@@ -1,18 +1,5 @@
 package coffeeMachine;
 
-import java.io.File;
-
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
-import java.io.File;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
-import javax.xml.bind.Unmarshaller;
-
 public class Main {
 	
 	
